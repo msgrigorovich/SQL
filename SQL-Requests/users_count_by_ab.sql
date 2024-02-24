e49b199a38d0c0acaaeb9c91de108f1c004167fd
Diana Grigorovich <107698943+msgrigorovich@users.noreply.github.com>
2024-02-24 03:42:44 +0300

    ### Updated: users_count_by_ab.sql

--- SQL-Requests/users_count_by_ab.sql
+++ SQL-Requests/users_count_by_ab.sql
@@ -1,5 +1,5 @@
 SELECT	app_version,
-ab_group,
+        ab_group,
 count (distinct user_id) AS users_unique -- user_id_example
 FROM ProductData.ProjectName_product -- database_and_tableview_example
 WHERE  ab_group LIKE '%021%' -- ab_group_number_example
