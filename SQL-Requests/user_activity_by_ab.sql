7a6e19ed154cbad08deeaab92150138cd927d6a7
Diana Grigorovich <107698943+msgrigorovich@users.noreply.github.com>
2024-02-24 03:38:56 +0300

    ### Changed: user_activity_by_ab.sql

--- SQL-Requests/user_activity_by_ab.sql
+++ SQL-Requests/user_activity_by_ab.sql
@@ -1,9 +1,9 @@
 SELECT 	created_at,
-		activity_kind,
-		event_name,
-		app_name,
-		app_version,
-		base_parameters -- global_events_example; used in json
+activity_kind,
+event_name,
+app_name,
+app_version,
+base_parameters -- global_events_example; used in json
 FROM AdjustData.TableViewExample -- database_and_tableview_example
 WHERE user_id <> 'EF8C20B5-6CBD-4EF3-A3A5-ADDFCA1DF335' -- user_id_example
 AND toDate(created_at) >= today()
